6dac790065e442b132027833ccbbcda83465b4e7
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-01-05 14:41:35 +0330

    first files added

---
+++ typio/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""typio modules."""
+from typio.params import TYPIO_VERSION
+
+__version__ = TYPIO_VERSION
---
+++ typio/params.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+"""typio params."""
+
+TYPIO_VERSION = "0.1"
