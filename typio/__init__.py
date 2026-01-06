6806af52a9eaea10b8c62315a02cbe250067d722
Sepand Haghighi <sepand.haghighi@yahoo.com>
2026-01-06 11:10:59 +0330

    Base (#1)
    * first files added
    
    * doc : README.md updated
    
    * feat : base code added

---
+++ typio/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""typio modules."""
+from .params import TYPIO_VERSION
+from .functions import TypeMode, TypioError
+from .functions import type_print, typestyle
+
+__version__ = TYPIO_VERSION
