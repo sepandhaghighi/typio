1b3972cc0ee982f4932906732f0ad99de6daab69
Sepand Haghighi <sepand.haghighi@yahoo.com>
2026-02-05 07:08:54 +0330

    Minor Edits (#16)
    * fix : jitter test section updated
    
    * doc : CHANGELOG.md updated
    
    * fix : jitter test section updated
    
    * fix : jitter test section updated

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -40,14 +40,14 @@ def test_line_mode():
 def test_sentence_mode():
     buffer = io.StringIO()
     text = "Hello! How are you?"
-    type_print(text, file=buffer, delay=0, mode=TypeMode.SENTENCE)
+    type_print(text, file=buffer, delay=1, jitter=0, mode=TypeMode.SENTENCE)
     assert buffer.getvalue() == text + "\n"
 
 
 def test_typewriter_mode():
     buffer = io.StringIO()
     text = "Hello\nWorld\n"
-    type_print(text, file=buffer, delay=0, mode=TypeMode.TYPEWRITER)
+    type_print(text, file=buffer, delay=1, jitter=0.05, mode=TypeMode.TYPEWRITER)
     assert buffer.getvalue() == text + "\n"
 
 
