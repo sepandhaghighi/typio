170b6b55b4b7148e0a980bdf933cc3a7bd3729e7
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-02-19 15:35:54 +0330

    fix : codacy issue fixed

--- typio/params.py
+++ typio/params.py
@@ -22,6 +22,7 @@ class TypeMode(Enum):
     TYPEWRITER = "typewriter"
     ADAPTIVE = "adaptive"
 
+
 EXIT_MESSAGE = "See you. Bye!"
 INVALID_TEXT_ERROR = "`text` must be str or bytes."
 INVALID_BYTE_ERROR = "bytes text must be UTF-8 decodable."
