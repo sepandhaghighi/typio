1437105c475c5fe8922efc56f5334ad5b327dee4
Sepand Haghighi <sepand.haghighi@yahoo.com>
2026-02-23 10:12:13 +0330

    Accelerate Mode (#32)
    * feat : _mode_accelerate method added
    
    * feat : TypeMode.ACCELERATE added
    
    * doc : README.md updated
    
    * fix : tests updated
    
    * fix : autopep8
    
    * doc : CHANGELOG.md updated

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -58,6 +58,13 @@ def test_adaptive_mode():
     assert buffer.getvalue() == text + "\n"
 
 
+def test_accelerate_mode():
+    buffer = io.StringIO()
+    text = "Hello, world!" * 100
+    type_print(text, file=buffer, delay=0.01, mode=TypeMode.ACCELERATE)
+    assert buffer.getvalue() == text + "\n"
+
+
 def test_default_stdout_capture(capsys):
     type_print("hello", delay=0)
     captured = capsys.readouterr()
