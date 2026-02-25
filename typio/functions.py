3d565e8e8cfe5f2c71d8009ea76ae01dce3957fe
Sepand Haghighi <sepand.haghighi@yahoo.com>
2026-02-25 06:57:58 +0330

    Refactor (#35)
    * fix : _mode_sentence method updated
    
    * fix : _mode_typewriter method updated

--- typio/functions.py
+++ typio/functions.py
@@ -159,7 +159,7 @@ class _TypioPrinter:
             self._emit(c)
             self._sleep()
             if c in ".!?":
-                self._sleep(self._delay * 4, self._jitter)
+                self._sleep(self._delay * 4)
 
     def _mode_typewriter(self, text: str) -> None:
         """
@@ -171,7 +171,7 @@ class _TypioPrinter:
             self._emit(c)
             self._sleep()
             if c == "\n":
-                self._sleep(self._delay * 5, self._jitter)
+                self._sleep(self._delay * 5)
 
     def _mode_adaptive(self, text: str) -> None:
         """
