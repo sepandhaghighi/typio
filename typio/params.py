9800afa74344959e41fe138ebd2169fed0acc8d7
Sadra Sabouri <sabouri.sadra@gmail.com>
2026-03-17 00:21:09 -0700

    add : key neighbors added.

--- typio/params.py
+++ typio/params.py
@@ -35,3 +35,27 @@ INVALID_MODE_ERROR = "`mode` must be a TypeMode enum value or a callable custom 
 INVALID_END_ERROR = "`end` must be a str."
 INVALID_FILE_ERROR = "`file` must be a file-like object."
 INVALID_NON_NEGATIVE_NUMBER_ERROR = "invalid non-negative number: '{value}'"
+
+KEY_NEIGHBORS = {
+    'q': 'wa12', 'w': 'qes23', 'e': 'wrd34', 'r': 'etf45', 't': 'ryg56', 'y': 'tuh67', 
+    'u': 'yij78', 'i': 'uok89', 'o': 'ipl90', 'p': 'ol0-[;', '[': 'p;\']-=', ']': '[\'=\\\n',
+    'a': 'qsz', 's': 'awdxz', 'd': 'serfcx', 'f': 'drtgvc', 'g': 'ftyhbv', 'h': 'gyujnb', 'j': 'huikmn', 
+    'k': 'jiolm', 'l': 'kop;,.', ';': 'lp\'./', '\'': ';/[\n', 'z': 'asx ', 'x': 'zsdc ', 'c': 'xdfv ', 
+    'v': 'cfgb ', 'b': 'vghn ', 'n': 'bhjm ', 'm': 'njk, ', ',': 'mkl. ', '.': ',l;\'/', '/': '.;\'',
+
+    'Q': 'WA!@', 'W': 'QES@#', 'E': 'WRD#$', 'R': 'ETF$%', 'T': 'RYG%^', 'Y': 'TUH^&', 
+    'U': 'YIJ&*', 'I': 'UOK*(', 'O': 'IPL()', 'P': 'OL)_+', '{': 'P:\"_+}', '}': '{+|\"',
+    'A': 'QSZ', 'S': 'AWDXZ', 'D': 'SERFCX', 'F': 'DRTGVC', 'G': 'FTYHBV', 'H': 'GYUJNB', 
+    'J': 'HUIKMN', 'K': 'JIOLM', 'L': 'KOP:<', ':': 'LP\">?', '\"': ':?{\n', 
+    'Z': 'ASX ', 'X': 'ZSDC ', 'C': 'XDFV ', 'V': 'CFGB ', 'B': 'VGHN ', 'N': 'BHJM ', 
+    'M': 'NJK< ', '<': 'MKL> ', '>': '<L:\"?', '?': '>:\";',
+
+    '1': 'q2`', '2': '13qw', '3': '24we', '4': '35er', '5': '46rt', '6': '57ty',
+    '7': '68yu', '8': '79ui', '9': '80io', '0': '9-op', '-': '0=p[', '=': '-][',
+
+    '!': 'Q@~', '@': '!#QW', '#': '@$WE', '$': '#%ER', '%': '$^RT', '^': '%&TY',
+    '&': '^*YU', '*': '&(UI', '(': '*)IO', ')': '(_OP', '_': ')+P{', '+': '_}{',
+    
+    ' ': 'cvbnm,./',
+    '\n': 'lp;\'[]\\'
+}
