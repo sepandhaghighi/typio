6716354af9c9ea741497fc5028ee0ccb8ce142f8
Sadra Sabouri <sabouri.sadra@gmail.com>
2026-04-16 22:55:55 -0700

    test : tests added.

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -150,6 +150,13 @@ def test_stutter_mode():
     assert buffer.getvalue() == 'Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, world!Hello, w-world!\n'
 
 
+def test_nervous_mode():
+    buffer = io.StringIO()
+    text = "Hello, world!"
+    type_print(text, file=buffer, delay=0.01, mode=TypeMode.NERVOUS)
+    assert buffer.getvalue() == 'Hello, world!\n'
+
+
 def test_default_stdout_capture(capsys):
     type_print("hello", delay=0)
     captured = capsys.readouterr()
