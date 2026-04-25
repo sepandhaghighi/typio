a5576fb16e14688702da3832eb26ea5259c29282
Sadra Sabouri <sabouri.sadra@gmail.com>
2026-04-25 14:12:17 -0700

    test : tests added.

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -180,6 +180,14 @@ def test_confident_mode():
     assert buffer.getvalue() == text + "\n"
 
 
+def test_echo_mode():
+    random.seed(1)
+    buffer = io.StringIO()
+    text = "Hello, world!"
+    type_print(text, file=buffer, delay=0.01, mode=TypeMode.ECHO)
+    assert buffer.getvalue() == "Hello, woorrld!\n"
+
+
 def test_default_stdout_capture(capsys):
     type_print("hello", delay=0)
     captured = capsys.readouterr()
