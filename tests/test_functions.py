d1e06d1a7eaac24d2fb7f63d4a90ae57457b3ef2
Sadra Sabouri <sabouri.sadra@gmail.com>
2026-05-07 15:25:39 -0700

    test : tests added for glide mode.

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -196,6 +196,13 @@ def test_drunk_mode():
     assert buffer.getvalue() == "Helloo, wworrld!Hello, wworld!Hello, orld!\n"
 
 
+def test_glide_mode():
+    buffer = io.StringIO()
+    text = "Hello, world!" * 3
+    type_print(text, file=buffer, delay=0.01, mode=TypeMode.GLIDE)
+    assert buffer.getvalue() == text + "\n"
+
+
 def test_default_stdout_capture(capsys):
     type_print("hello", delay=0)
     captured = capsys.readouterr()
