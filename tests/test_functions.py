5ef5bba2e0673bce4958b914fb65a54ba0788bf5
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-05-09 14:25:49 +0330

    fix : add focus-drift test

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -203,6 +203,13 @@ def test_glide_mode():
     assert buffer.getvalue() == text + "\n"
 
 
+def test_focus_drift_mode():
+    buffer = io.StringIO()
+    text = "Hello, world!"
+    type_print(text, file=buffer, delay=0.01, mode=TypeMode.FOCUS_DRIFT)
+    assert buffer.getvalue() == text + "\n"
+
+
 def test_default_stdout_capture(capsys):
     type_print("hello", delay=0)
     captured = capsys.readouterr()
