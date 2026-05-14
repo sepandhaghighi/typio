18c41727776525bab39188eb4050b44b40a628c9
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-05-14 14:23:15 +0330

    fix : update init file

--- typio/__init__.py
+++ typio/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """typio modules."""
 from .params import TYPIO_VERSION, TypeMode
-from .errors import TypioError
+from .errors import TypioError, TypioValidationError
 from .functions import type_print, typestyle, TypioContext
 
 __version__ = TYPIO_VERSION
