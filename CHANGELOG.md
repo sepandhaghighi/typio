acc2f0042e6151c99fb029cf03bfcfa9256524dc
Sepand Haghighi <sepand.haghighi@yahoo.com>
2026-05-16 08:06:39 +0330

    Validation Error (#81)
    * feat : add TypioValidationError class
    
    * doc : update CHANGELOG.md
    
    * fix : update validation error type
    
    * fix : update init file
    
    * fix : update tests
    
    * fix : autopep8
    
    * fix : update __init__.py

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - `DRUNK` mode
 - `GLIDE` mode
 - `FOCUS_DRIFT` mode
+- `TypioValidationError` class
 ### Changed
 - Test system modified
 - `README.md` updated
--- typio/__init__.py
+++ typio/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """typio modules."""
 from .params import TYPIO_VERSION, TypeMode
-from .errors import TypioError
+from .errors import TypioError, TypioValidationError
 from .functions import type_print, typestyle, TypioContext
 
 __version__ = TYPIO_VERSION
-__all__ = ["TypeMode", "TypioError", "type_print", "typestyle", "TypioContext"]
+__all__ = ["TypeMode", "TypioError", "TypioValidationError", "type_print", "typestyle", "TypioContext"]
