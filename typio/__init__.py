23ba1d7f0cc47a8bb8f524085652be5e6fc3a301
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-09-09 18:12:49 +0330

    fix : rename functions to core

--- typio/__init__.py
+++ typio/__init__.py
@@ -2,7 +2,7 @@
 """typio modules."""
 from .params import TYPIO_VERSION, TypeMode
 from .errors import TypioError, TypioValidationError
-from .functions import type_print, typestyle, TypioContext
+from .core import type_print, typestyle, TypioContext
 
 __version__ = TYPIO_VERSION
 __all__ = ["TypeMode", "TypioError", "TypioValidationError", "type_print", "typestyle", "TypioContext"]
--- typio/functions.py
+++ typio/functions.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""typio functions."""
+"""typio core."""
 
 import sys
 import time
